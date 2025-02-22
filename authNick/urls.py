a2a8a32de1a31f2362505014be40155e68a802d5
yosyp <kolomiets204@gmail.com>
2025-02-22 01:07:11 +0100

    added the main page and authorization without password

--- authNick/urls.py
+++ authNick/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include
-
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/",include("polls.urls")),
-    path("authNick/",include("authNick.urls")),
-]
+    path('', views.welcome_view),
 
+]
